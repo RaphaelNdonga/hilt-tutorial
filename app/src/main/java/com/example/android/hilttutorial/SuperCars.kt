63337387c6116b517e4f3dd504fb1a58b09f3e70
Raphael Ndonga <nkibi53@gmail.com>
2021-05-03 18:54:40 +0300

    git init

---
+++ app/src/main/java/com/example/android/hilttutorial/SuperCars.kt
@@ -0,0 +1,11 @@
+package com.example.android.hilttutorial
+
+class SuperCars {
+    private val superCarList = listOf<String>(
+        "Mercedes Benz",
+        "Bugatti",
+        "Mc Laren",
+        "Toyota Supra",
+        "Mazda RX8"
+    )
+}
